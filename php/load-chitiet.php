d14be949beb233705c3917d8a466a47ab08b720d
axetung2008 <54396362+axetung2008@users.noreply.github.com>
2020-11-11 10:05:36 +0700

    Add files via upload

--- php/load-chitiet.php
+++ php/load-chitiet.php
@@ -5,17 +5,17 @@
 
 	$madt = $_POST['newid'];
 
-    $chitiet = "SELECT * FROM chi_tiet_don_thuoc WHERE ma_don_thuoc='$madt'";
-    $result_chitiet = mysqli_query($conn,$chitiet);
-    $i = 1;
+  $chitiet = "SELECT * FROM chi_tiet_don_thuoc WHERE ma_don_thuoc='$madt'";
+  $result_chitiet = mysqli_query($conn,$chitiet);
+  $i = 1;
 
     while($row=mysqli_fetch_assoc($result_chitiet)){
-    	echo '<tr>';
+      echo '<tr>';
     	echo '<th>' . $i . '</th>';
-    	echo '<th style="padding-left: 30px" id="tenthuoc'.$i.'">';
-    	echo '<h4>'.$row['ten_thuoc'].'</h4>';
+    	echo '<th style="padding-left: 30px">';
+    	echo '<h4 id="tenthuoc'.$i.'">'.$row['ten_thuoc'].'</h4>';
     	echo '</th>';
-    	echo '<th>' . $row['so_luong'] . '</th>';
+    	echo '<th style="padding-left: 100px" id="solg'.$i.'">' . $row['so_luong'] . '</th>';
     	echo '</tr>';
     	echo '<tr>';
     	echo '<th>' . "Cách uống" . '</th>';
@@ -28,7 +28,7 @@
         if($re[0] != ""){
         	$cach = "Sáng"." ".$re[0];
        		$count++;
- 		}
+ 		    }
         if($re[1] != ""){
         	if($count > 1){
         	$cach = $cach . " - ";
@@ -44,58 +44,19 @@
         	$count++;
         }
     	echo $cach;
-        echo '</th>';
-    // 	echo '<p hidden id="s' . $i . '">' . $re[0] . '</p>' .
-    //      	 '<p hidden id="c' . $i . '">' . $re[1] . '</p>' .
-			 // '<p hidden id="t' . $i . '">' . $re[2] . '</p>' .
-			 // 	'</th>' .
+      echo '</th>';
+      echo '<tr>';
+    	echo '<th hidden id="s' . $i . '">' . $re[0] . '</th>' .
+         	 '<th hidden id="c' . $i . '">' . $re[1] . '</th>' .
+			     '<th hidden id="t' . $i . '">' . $re[2] . '</th>';
+			echo '<tr>';
+
     	echo '</tr>';
+      echo '<tr>';
     	$i++;
     }	
-
-   //  	echo '<th>' . $i . '</th>' .
-   //  		 '<th style="padding-left: 30px" id="tenthuoc'. $i . '">' .
-   //  		 $row['ten_thuoc'] .
-   //  		 '</th>' .
-   //  		 '<th style="padding-left: 100px" id="solg' . $i . '">' .
-   //  		 $row['so_luong'] .
-   //  		 '</th>' .
-   //  		 '<tr>' .
-	  //   		 '<th>' . "Cách uống" . '</th>' .
-	  //   		 '<th style="padding-left: 30px">';
-   //  	$string = $row['cach_uong'];
-   //      $pattern = "/[\/]/";
-   //  	$re = preg_split($pattern, $string);
-   //      $cach = "";
-   //      $count = 1;
-   //      if($re[0] != ""){
-   //      	$cach = "Sáng"." ".$re[0];
-   //     		$count++;
- 		// }
-   //      if($re[1] != ""){
-   //      	if($count > 1){
-   //      	$cach = $cach . " - ";
-   //      }
-   //      $cach = $cach."Chiều"." ".$re[1];
-   //      $count++;
-   //      }
-   //      if($re[2] != ""){
-   //      	if($count > 1){
-   //     			$cach = $cach . " - ";
-   //    		}
-   //      	$cach = $cach."Tối"." ".$re[2];
-   //      	$count++;
-   //      }
-   //  	echo $cach;
-   //  	echo '<p hidden id="s' . $i . '">' . $re[0] . '</p>' .
-   //       	 '<p hidden id="c' . $i . '">' . $re[1] . '</p>' .
-			//  '<p hidden id="t' . $i . '">' . $re[2] . '</p>' .
-			//  	'</th>' .
-			//  '</tr>';
-   //      $i++;
-
-   //      echo '</tr>';
-   //  }
-   //  echo '<p hidden id="count">' . $i . '</p>';
+  echo '<tr hidden">';
+  echo '<th hidden id="count">' . $i . '</th>';
+  echo '</tr>';
 
 ?>
